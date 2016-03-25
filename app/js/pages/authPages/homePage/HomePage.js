b20a0ee002b3511fabff003be912ce2ae46d51a0
lucky ZHOU <lucky.zhou@xplusz.com>
2016-03-25 17:52:06 +0800

    Update version 4 to test api

--- app/js/pages/authPages/homePage/HomePage.js
+++ app/js/pages/authPages/homePage/HomePage.js
@@ -13,7 +13,7 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div className={styles.homeContainer}>
-                Home Page! (Updated 3)!
+                Home Page! (Updated 4)!
             </div>
         );
     }
